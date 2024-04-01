97f4d538c445d212a9ad138257978e02391125c7
Zachary Kokot <zachary.kokot@ucalgary.ca>
2024-04-01 16:19:20 -0600

    Improved efficiency by reducing the frequency the ui updates.

--- source/random.c
+++ source/random.c
@@ -25,7 +25,7 @@ inline int rand()
 #define RAND_MAX_32 ((1U << 31) - 1)
 #define RAND_MAX ((1U << 15) - 1)
 
-inline int rand()
+inline int rand(int rseed)
 {
 	return (rseed = (rseed * 214013 + 2531011) & RAND_MAX_32) >> 16;
 }
